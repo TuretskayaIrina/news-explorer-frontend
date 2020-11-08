a727396182755df6d79bffe49b4a2d8dd1e480f8
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-11-08 21:07:37 +0300

    feat: footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Footer from '../Footer/Footer'
 
 function App() {
+
   return (
     <div className="App">
-      Hello world!
+      <Footer />
     </div>
   );
 }
