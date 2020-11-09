7b16d7171f5b73b66bf6df2d776118119bd97855
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-11-09 20:48:56 +0300

    feat: section about author

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Footer from '../Footer/Footer'
+import About from '../About/About';
+import Footer from '../Footer/Footer';
 
 function App() {
 
   return (
     <div className="App">
+      <About />
       <Footer />
     </div>
   );
