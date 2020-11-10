5d92351b812b7ed370d776abcba12d6892d98619
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-11-10 17:03:47 +0300

    feat: component Preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,12 @@
+import './Preloader.css';
+import React from 'react';
+
+function Preloader() {
+    return(
+      <body>
+          <i class="circle-preloader"></i>
+      </body>
+    );
+}
+
+export default Preloader;
